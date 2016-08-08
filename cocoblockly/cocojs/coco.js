646df2675f04c2a0d0f16703af11a3b754dcb2cf
xcorex <xcorex@MANTICORE.local>
2016-08-08 22:20:05 +0700

    refactor js

--- cocoblockly/cocojs/coco.js
+++ cocoblockly/cocojs/coco.js
@@ -27,46 +27,46 @@ CocoBlockly.tabClick = function(clickedName) {
 };
 
 
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function load(event) {
+    window.removeEventListener('load', load, false);
+    // var toolBox =  Blockly.Xml.textToDom(Ardublockly.TOOLBOX_XML);
+    CocoBlockly.toolBox = document.getElementById('ArduBlocklyToolbox');
+    // var defaultBlocks = document.getElementById('blocklyDefault');
 
-// var toolBox =  Blockly.Xml.textToDom(Ardublockly.TOOLBOX_XML);
-CocoBlockly.toolBox = document.getElementById('ArduBlocklyToolbox');
-// var defaultBlocks = document.getElementById('blocklyDefault');
-
-CocoBlockly.workspace = Blockly.inject('blocklyDiv',
-  {
-    css: false,
-    toolbox: CocoBlockly.toolBox,
-    media: 'blockly/',
-        grid: {
-            spacing: 25,
-            length: 3,
-            colour: "#ddd",
-            snap: !0
-        },
-        zoom: {
-            controls: !0,
-            wheel: !1,
-            startScale: 1,
-            maxScale: 3,
-            minScale: .3,
-            scaleSpeed: 1.2
-        },
-        trashcan: true
-  });
+    CocoBlockly.workspace = Blockly.inject('blocklyDiv',
+    {
+        css: false,
+        toolbox: CocoBlockly.toolBox,
+        media: 'blockly/',
+            grid: {
+                spacing: 25,
+                length: 3,
+                colour: "#ddd",
+                snap: !0
+            },
+            zoom: {
+                controls: !0,
+                wheel: !1,
+                startScale: 1,
+                maxScale: 3,
+                minScale: .3,
+                scaleSpeed: 1.2
+            },
+            trashcan: true
+    });
 
 
-CocoBlockly.CodeMirror = CodeMirror.fromTextArea(document.getElementById("codeDiv"), {
-  lineNumbers: true,
-  lineWrapping: true,
-  mode: "text/x-c++src"
+    CocoBlockly.CodeMirror = CodeMirror.fromTextArea(document.getElementById("codeDiv"), 
+    {
+      lineNumbers: true,
+      lineWrapping: true,
+      readOnly: true,
+      mode: "text/x-c++src"
+    });
+                              
+    CocoBlockly.workspace.addChangeListener(CocoBlockly.myUpdateFunction);
+
+    document.getElementById("btn-mode-blocks").onclick = function(){CocoBlockly.tabClick('blocks')};
+    document.getElementById("btn-mode-code").onclick = function(){CocoBlockly.tabClick('code')};
+    document.getElementById("btn-mode-xml").onclick = function(){CocoBlockly.tabClick('xml')};
 });
-                          
-CocoBlockly.workspace.addChangeListener(CocoBlockly.myUpdateFunction);
-
-document.getElementById("btn-mode-blocks").onclick = function(){CocoBlockly.tabClick('blocks')};
-document.getElementById("btn-mode-code").onclick = function(){CocoBlockly.tabClick('code')};
-document.getElementById("btn-mode-xml").onclick = function(){CocoBlockly.tabClick('xml')};
-
-
-}, false);
