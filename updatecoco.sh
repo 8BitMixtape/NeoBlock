926bb70b9a0d5ef662eec8226ae1082856be6c53
xcorex <xcorex@MANTICORE.local>
2016-08-08 19:33:17 +0700

    refactor

---
+++ updatecoco.sh
@@ -0,0 +1,4 @@
+cp ./blockly/arduino_compressed.js ./cocoblockly/blockly/
+cp ./blockly/blockly_compressed.js ./cocoblockly/blockly/
+cp ./blockly/blocks_compressed.js ./cocoblockly/blockly/
+cp ./blockly/msg/js/en.js ./cocoblockly/blockly/
