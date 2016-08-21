8319cc41fd56b9d966408fc3c63a1fe3b6b8bf46
Budi Prakosa <iyok@deadmediafm.org>
2016-08-21 11:46:54 +0700

    add socket io rpc support

--- cocoblockly/cocojs/coco.js
+++ cocoblockly/cocojs/coco.js
@@ -1,6 +1,8 @@
 
 var CocoBlockly = CocoBlockly || {};
 
+CocoBlockly.code = "";
+
 CocoBlockly.executeBlockCode = function() {
   var code = Blockly.Arduino.workspaceToCode(CocoBlockly.workspace);
   console.log('play');
@@ -9,6 +11,9 @@ CocoBlockly.executeBlockCode = function() {
 CocoBlockly.myUpdateFunction = function(event) {
   var doc = CocoBlockly.CodeMirror.getDoc();
   var code = Blockly.Arduino.workspaceToCode(CocoBlockly.workspace)
+
+  CocoBlockly.code = code;
+
   CocoBlockly.CodeMirror.getDoc().setValue(code)
   CocoBlockly.CodeMirrorPreview.getDoc().setValue(code)
   
@@ -29,7 +34,7 @@ CocoBlockly.tabClick = function(clickedName) {
     document.getElementById("pane-blocks").className = "tab-pane active";
     document.getElementById("pane-xml").className = "tab-pane";    
     document.getElementById("pane-code").className = "tab-pane";
-    document.getElementById("pane-midi").className = "tab-pane";
+    document.getElementById("pane-console").className = "tab-pane";
 
     document.body.className = "simulator";
   }
@@ -39,7 +44,7 @@ CocoBlockly.tabClick = function(clickedName) {
     document.getElementById("pane-blocks").className = "tab-pane";
     document.getElementById("pane-xml").className = "tab-pane";    
     document.getElementById("pane-code").className = "tab-pane active";
-    document.getElementById("pane-midi").className = "tab-pane";
+    document.getElementById("pane-console").className = "tab-pane";
 
     document.body.className = "";
 
@@ -50,23 +55,102 @@ CocoBlockly.tabClick = function(clickedName) {
     document.getElementById("pane-blocks").className = "tab-pane";
     document.getElementById("pane-code").className = "tab-pane";
     document.getElementById("pane-xml").className = "tab-pane active";
-    document.getElementById("pane-midi").className = "tab-pane";
+    document.getElementById("pane-console").className = "tab-pane";
 
     document.body.className = "";
   }
 
 
-  if (clickedName == 'midi') {
+  if (clickedName == 'console') {
     CocoBlockly.workspace.setVisible(false);
     document.getElementById("pane-blocks").className = "tab-pane";
     document.getElementById("pane-code").className = "tab-pane";
     document.getElementById("pane-xml").className = "tab-pane";
-    document.getElementById("pane-midi").className = "tab-pane active";
+    document.getElementById("pane-console").className = "tab-pane active";
 
     document.body.className = "";
   }
 
 };
+
+
+CocoBlockly.setProgressBar = function(value) {
+  $('.progress-bar').css('width', value+'%').attr('aria-valuenow', value);
+}
+
+CocoBlockly.rpc = {};
+CocoBlockly.rpc.socket = io("http://localhost:3000");
+CocoBlockly.rpc.globalCounter = 0;
+
+CocoBlockly.rpc.processRpcBroadcast = function(data)
+{ 
+var command = data['command'];
+var params = data['params'];
+
+switch (command) {
+  case 'console':
+    // console.log(params);
+    CocoBlockly.CodeMirrorConsole.replaceRange(params, {line: Infinity});
+    CocoBlockly.CodeMirrorConsole.setCursor(CocoBlockly.CodeMirrorConsole.lastLine());
+    break;
+  case 'progress':
+    if (params === '100'  ) {
+      CocoBlockly.setProgressBar(100);
+      setTimeout(function(){CocoBlockly.setProgressBar(0); }, 1000);
+    }else {
+      CocoBlockly.setProgressBar(params);              
+    }
+    break;
+  default:
+    break;
+}
+}
+
+CocoBlockly.rpc.socket.on('statusRPC', function(msg){
+  CocoBlockly.rpc.processRpcBroadcast(msg);
+});
+
+CocoBlockly.rpc.sendBasicRpc = function(command, fun)
+{
+ var localCallCounter = ++CocoBlockly.rpc.globalCounter;
+ CocoBlockly.rpc.socket.once('doneRPC' + localCallCounter, function (data) {
+     fun ( data );
+ });
+ CocoBlockly.rpc.socket.emit('doRPC', localCallCounter, command);
+}
+
+CocoBlockly.rpc.sendRpcParam = function (cmd, param, fun)
+{
+  var command = {command: cmd};
+  command.params = param;
+  CocoBlockly.rpc.sendBasicRpc(command, fun);
+}
+
+CocoBlockly.rpcUploadCode = function(code, fun)
+{
+  CocoBlockly.rpc.sendRpcParam('upload', '', fun);
+}
+
+CocoBlockly.rpcCompileCode = function(code, fun)
+{
+  CocoBlockly.rpc.sendRpcParam('compile', code, fun);
+}
+
+CocoBlockly.upload = function()
+{
+  CocoBlockly.CodeMirrorConsole.getDoc().setValue("");
+  CocoBlockly.rpc.sendRpcParam('upload', '', function(){
+    console.log('upload done..');
+  });
+}
+
+CocoBlockly.compile = function()
+{
+  CocoBlockly.CodeMirrorConsole.getDoc().setValue("");
+  CocoBlockly.rpcCompileCode(CocoBlockly.code, function(){
+    console.log('compile done..');
+  });
+}
 
 
 window.addEventListener('load', function load(event) {
@@ -126,13 +210,24 @@ window.addEventListener('load', function load(event) {
       readOnly: true,
       mode: "application/xml"
     });
+
+    CocoBlockly.CodeMirrorConsole = CodeMirror.fromTextArea(document.getElementById("consoleDiv"), 
+    {
+      lineNumbers: true,
+      lineWrapping: true,
+      readOnly: true,
+      mode: "application/xml"
+    });
                               
     CocoBlockly.workspace.addChangeListener(CocoBlockly.myUpdateFunction);
 
     document.getElementById("btn-mode-blocks").onclick = function(){CocoBlockly.tabClick('blocks')};
     document.getElementById("btn-mode-code").onclick = function(){CocoBlockly.tabClick('code')};
     document.getElementById("btn-mode-xml").onclick = function(){CocoBlockly.tabClick('xml')};
-    document.getElementById("btn-mode-midi").onclick = function(){CocoBlockly.tabClick('midi')};
+    document.getElementById("btn-mode-console").onclick = function(){CocoBlockly.tabClick('console')};
+    document.getElementById("btn-mode-compile").onclick = function(){CocoBlockly.compile()};
+    document.getElementById("btn-mode-upload").onclick = function(){CocoBlockly.upload()};
+
 
     var svgresize = function() {
         Blockly.svgResize(CocoBlockly.workspace);
@@ -143,5 +238,6 @@ window.addEventListener('load', function load(event) {
         minSize: 200,
         onDragEnd: svgresize
     });
+
 
 });
