ea2660340d4f48b91a5fa1f0e338e1b615200f7b
Budi Prakosa <iyok@deadmediafm.org>
2018-04-26 11:02:34 +0700

    refactor example module

--- cocoblockly/neosrc/NeoModules/NeoLight/setup_block.js
+++ cocoblockly/neosrc/NeoModules/NeoLight/setup_block.js
@@ -1,3 +1,31 @@
+/*
+
+    //block interface
+    this.appendDummyInput()
+    .appendField("NeoLight count ")
+    .appendField(new Blockly.FieldTextInput("0"), "NEOLIGHT_COUNT");
+
+    //read
+    get_field_value(block, "NEOLIGHT_COUNT")
+
+    //block interface
+    this.appendDummyInput()
+    .appendField("setup for voice channel")
+    .appendField(new Blockly.FieldDropdown([["0", "0"], ["1", "1"], ["2", "2"], ["3", "3"]]), "COCO_VOICE");
+
+    //read
+    get_field_value(block, "COCO_VOICE")
+
+    //block interface
+    this.appendValueInput("COCO_MOD")
+    .setCheck(null)
+    .appendField("modulation to");
+
+    //read
+    get_field_value_atomic(block, "COCO_MOD")
+
+*/
+
 Blockly.Blocks['NeoLight_setup'] = {
     init: function() {
       this.appendDummyInput()
