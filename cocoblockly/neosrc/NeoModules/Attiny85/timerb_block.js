50e924d3c9332546dbf501613c63a98dcbe8ae1e
Budi Prakosa <iyok@deadmediafm.org>
2018-05-05 06:39:21 +0700

    better timerb block

--- cocoblockly/neosrc/NeoModules/Attiny85/timerb_block.js
+++ cocoblockly/neosrc/NeoModules/Attiny85/timerb_block.js
@@ -55,7 +55,35 @@ Blockly.Blocks['NeoLib_timerb'] = {
               ["CTC Disable", "0"], 
               ["CTC Enable", "1"]       
             ]), "CTC_ENABLE");        
+
             
+        this.appendDummyInput()
+        .appendField("Timer/Counter1 Prescale")
+        .appendField(new Blockly.FieldDropdown([
+            ["Default", "default"],                   
+            ["T/C1 stopped", "0"], 
+            ["PCK", "1"], 
+            ["PCK/2", "2"],
+            ["PCK/4", "3"],
+            ["PCK/8", "4"],
+            ["PCK/16", "5"],
+            ["PCK/32", "6"],
+            ["PCK/64", "7"], 
+            ["PCK/128", "8"],
+            ["PCK/256", "9"],
+            ["PCK/512", "10"],
+            ["PCK/1024", "11"],
+            ["PCK/2028", "12"],
+            ["PCK/4096", "13"],
+            ["PCK/8192", "14"],
+            ["PCK/16384", "15"],
+          ]), "PRESCALE_SELECT");
+
+
+          this.appendDummyInput()
+          .appendField("Setup Comparator A");
+
+      
             
             this.appendDummyInput()
             .appendField("Enable PWM A (OCR1A)")
@@ -81,38 +109,7 @@ Blockly.Blocks['NeoLib_timerb'] = {
               ["[pwm] OC1x Set on compare match/_OC1x_ not connected", "7"]                           
             ]), "COMPARATOR_MODE_NORMAL");       
 
-        this.appendDummyInput()
-            .appendField("Compare Output Mode")
-            .appendField(new Blockly.FieldDropdown([
-                ["Default", "default"],                 
-                ["Normal port operation, OC0A/OC0B disconnected.", "0"], 
-                ["Clear OC0A/OC0B on Compare Match, set OC0A/OC0B at BOTTOM (non-inverting mode)", "2"], 
-                ["Set OC0A/OC0B on Compare Match, clear OC0A/OC0B at BOTTOM (inverting mode)", "3"]         
-              ]), "COMPARE_MODE");
               
-        this.appendDummyInput()
-              .appendField("Timer/Counter1 Prescale")
-              .appendField(new Blockly.FieldDropdown([
-                  ["Default", "default"],                   
-                  ["T/C1 stopped", "0"], 
-                  ["PCK", "1"], 
-                  ["PCK/2", "2"],
-                  ["PCK/4", "3"],
-                  ["PCK/8", "4"],
-                  ["PCK/16", "5"],
-                  ["PCK/32", "6"],
-                  ["PCK/64", "7"], 
-                  ["PCK/128", "8"],
-                  ["PCK/256", "9"],
-                  ["PCK/512", "10"],
-                  ["PCK/1024", "11"],
-                  ["PCK/2028", "12"],
-                  ["PCK/4096", "13"],
-                  ["PCK/8192", "14"],
-                  ["PCK/16384", "15"],
-                ]), "PRESCALE_SELECT");
-
-
                 this.appendDummyInput()
                 .appendField("Setup Comparator B");
       
