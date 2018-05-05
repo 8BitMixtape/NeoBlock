b76e22015741140d13bdac245f81ddf5eb70d993
Budi Prakosa <iyok@deadmediafm.org>
2018-05-06 06:32:42 +0700

    add arduino download toolchain command

--- build-neoblock.sh
+++ build-neoblock.sh
@@ -2,6 +2,37 @@ cd cocoblockly
 npm install
 gulp xml
 gulp
+
+#osx
+unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/hardware/tools/*"
+unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/arduino-builder"
+mv "Arduino.app/Contents/Java/hardware/tools" ./tools_osx
+mv "Arduino.app/Contents/Java/arduino-builder" ./tools_osx
+rm -fr Arduino.app
+
+#windows
+#wget https://downloads.arduino.cc/arduino-1.8.4-windows.zip
+#unzip arduino-1.8.4-windows.zip "arduino-1.8.4/arduino-builder.exe"
+#unzip arduino-1.8.4-windows.zip "arduino-1.8.4/hardware/tools/*"
+#mv arduino-1.8.4/hardware/tools ./tools_win
+#mv arduino-1.8.4/arduino-builder.exe ./tools_win
+#rm -fr arduino-1.8.4
+
+#linux 64
+#wget https://downloads.arduino.cc/arduino-1.8.4-linux64.tar.xz
+#tar xf arduino-1.8.4-linux64.tar.xz
+#mkdir tools_linux64
+#mv arduino-1.8.4/arduino-builder  ./tools_linux64
+#mv arduino-1.8.4/hardware/tools/*  ./tools_linux64
+
+#linux 32
+#wget https://downloads.arduino.cc/arduino-1.8.4-linux32.tar.xz
+#tar xf arduino-1.8.4-linux32.tar.xz
+#mkdir tools_linux32
+#mv arduino-1.8.4/arduino-builder  ./tools_linux32
+#mv arduino-1.8.4/hardware/tools/*  ./tools_linux32
+
+
 npm run buildosx
 npm run buildwin
 npm run buildlinux
