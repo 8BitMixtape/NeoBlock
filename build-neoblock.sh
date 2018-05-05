f2c36803a765abade6cc86fdb7a48a1d157f1f94
Budi Prakosa <iyok@deadmediafm.org>
2018-05-05 14:16:32 +0700

    fix xml and win build

--- build-neoblock.sh
+++ build-neoblock.sh
@@ -1,9 +1,10 @@
 cd cocoblockly
 npm install
+gulp xml
 gulp
 npm run buildosx
 npm run buildwin
 ls -al
 tar -czvf NeoBlock-darwin-x64
 tar -czvf NeoBlock-darwin-x64.tar.gz NeoBlock-darwin-x64/
-tar -czvf NeoBlock-win32-x32.tar.gz NeoBlock-win32-x32/
+tar -czvf NeoBlock-win32-ia32.tar.gz NeoBlock-win32-ia32/
