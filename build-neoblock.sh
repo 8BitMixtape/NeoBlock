14b63440b44a421c1070a1c99e6aa308f485448c
Budi Prakosa <iyok@deadmediafm.org>
2018-05-05 14:35:02 +0700

    add linux build

--- build-neoblock.sh
+++ build-neoblock.sh
@@ -4,7 +4,8 @@ gulp xml
 gulp
 npm run buildosx
 npm run buildwin
+npm run buildlinux
 ls -al
-tar -czvf NeoBlock-darwin-x64
+tar -czvf NeoBlock-linux-ia32.tar.gz NeoBlock-linux-ia32/
 tar -czvf NeoBlock-darwin-x64.tar.gz NeoBlock-darwin-x64/
 zip NeoBlock-win32-ia32.zip -r NeoBlock-win32-ia32/
