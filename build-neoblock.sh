8d1a52e52e52741bc6b36bbb10980dd35cbf7535
Budi Prakosa <iyok@deadmediafm.org>
2018-05-05 13:36:50 +0700

    deploy to release

--- build-neoblock.sh
+++ build-neoblock.sh
@@ -2,4 +2,8 @@ cd cocoblockly
 npm install
 gulp
 npm run buildosx
+npm run buildwin
 ls -al
+tar -czvf NeoBlock-darwin-x64
+tar -czvf NeoBlock-darwin-x64.tar.gz NeoBlock-darwin-x64/
+tar -czvf NeoBlock-win32-x32.tar.gz NeoBlock-darwin-x64/
