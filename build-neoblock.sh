b6b95a83c247a6215601a0c28b64c73d9f845215
Budi Prakosa <iyok@deadmediafm.org>
2018-05-06 14:43:04 +0700

    refactor build system

--- build-neoblock.sh
+++ build-neoblock.sh
@@ -12,47 +12,65 @@ wget https://raw.githubusercontent.com/8BitMixtape/8BitMixtape.github.io/master/
 tar -zxvf 8bitmixtape-0.0.28.tar.gz -C temp
 mv temp/avr toolchain/platform/8BitMixtape/hardware/avr/0.0.28
 
-#osx
-mkdir -p toolchain/tools_osx
+#cross platform toolchain
+mkdir -p tools_osx
+mkdir -p tools_win
+mkdir -p tools_linux64
+
 wget https://downloads.arduino.cc/arduino-1.8.4-macosx.zip
+
+unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/libraries/*"
+mv "Arduino.app/Contents/Java/libraries" ./toolchain/libraries
+
 unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/hardware/*"
 unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/tools-builder/*"
-unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/libraries/*"
 unzip arduino-1.8.4-macosx.zip "Arduino.app/Contents/Java/arduino-builder"
-mv "Arduino.app/Contents/Java/hardware/" ./toolchain/tools_osx
-mv "Arduino.app/Contents/Java/tools-builder" ./toolchain/tools_osx
-mv "Arduino.app/Contents/Java/arduino-builder" ./toolchain/tools_osx
-mv "Arduino.app/Contents/Java/libraries" ./toolchain/libraries
+mv "Arduino.app/Contents/Java/hardware/" ./tools_osx
+mv "Arduino.app/Contents/Java/tools-builder" ./tools_osx
+mv "Arduino.app/Contents/Java/arduino-builder" ./tools_osx
 rm -fr Arduino.app
 
 #windows
 #wget https://downloads.arduino.cc/arduino-1.8.4-windows.zip
 #unzip arduino-1.8.4-windows.zip "arduino-1.8.4/arduino-builder.exe"
-#unzip arduino-1.8.4-windows.zip "arduino-1.8.4/hardware/tools/*"
-#mv arduino-1.8.4/hardware/tools ./tools_win
+#unzip arduino-1.8.4-windows.zip "arduino-1.8.4/tools-builder"
+#unzip arduino-1.8.4-windows.zip "arduino-1.8.4/hardware/*"
+#mv arduino-1.8.4/hardware ./tools_win
+#mv arduino-1.8.4/tools-builder ./tools_win
 #mv arduino-1.8.4/arduino-builder.exe ./tools_win
 #rm -fr arduino-1.8.4
 
 #linux 64
 #wget https://downloads.arduino.cc/arduino-1.8.4-linux64.tar.xz
 #tar xf arduino-1.8.4-linux64.tar.xz
-#mkdir tools_linux64
+#mv arduino-1.8.4/hardware  ./tools_linux64
+#mv arduino-1.8.4/tools-builder  ./tools_linux64
 #mv arduino-1.8.4/arduino-builder  ./tools_linux64
-#mv arduino-1.8.4/hardware/tools/*  ./tools_linux64
+#rm -fr arduino-1.8.4
 
 #linux 32
 #wget https://downloads.arduino.cc/arduino-1.8.4-linux32.tar.xz
 #tar xf arduino-1.8.4-linux32.tar.xz
-#mkdir tools_linux32
+#mv arduino-1.8.4/hardware  ./tools_linux32
+#mv arduino-1.8.4/tools-builder  ./tools_linux32
 #mv arduino-1.8.4/arduino-builder  ./tools_linux32
-#mv arduino-1.8.4/hardware/tools/*  ./tools_linux32
-
+#rm -fr arduino-1.8.4
 
 npm run buildosx
 #npm run buildwin
 #npm run buildlinux
 
 cp -r toolchain NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources
+cp -r tools_osx NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources/toolchain
+
+#windows
+#cp -r toolchain NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources
+#cp -r tools_osx NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources/toolchain
+
+#linux
+#cp -r toolchain NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources
+#cp -r tools_osx NeoBlock-darwin-x64/NeoBlock.app/Contents/Resources/toolchain
+
 
 #ls -al
 #tar -czvf NeoBlock-linux-ia32.tar.gz NeoBlock-linux-ia32/
